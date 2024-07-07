b0a8beac6e0049de2f3a1ed3d77b82fcfac22522
Joshua <126733588+AshurVT@users.noreply.github.com>
2024-07-06 20:15:16 -0700

    Update Note.jsx

--- jde239 assignment 5/src/components/Note.jsx
+++ jde239 assignment 5/src/components/Note.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Note () {
   return <div className="note">
-  <h1> Header </h1> 
-  <p> Paragraph </p>
+  <h1> This is my Keeper App Header </h1> 
+  <p> This is where all the information would go.... IF I HAD ANY. </p>
   </div>
 }
 
